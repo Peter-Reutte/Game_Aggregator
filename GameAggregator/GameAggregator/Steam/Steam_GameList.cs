d43e79cc6e9d666403ff94905e04ce8e432ef38e
Ксения Быстрова <ksenya_bystrova@mail.ru>
2020-05-09 13:43:37 +0500

    Добавлен функционал для рабобты со Steam API

---
+++ GameAggregator/GameAggregator/Steam/Steam_GameList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace GameAggregator
+{
+    public class Game
+    {
+        public int Appid { get; set; }
+        public string Name { get; set; }
+        public int Playtime_forever { get; set; }
+        public string Img_icon_url { get; set; }
+        public string Img_logo_url { get; set; }
+        public bool Has_community_visible_stats { get; set; }
+        public int Playtime_windows_forever { get; set; }
+        public int Playtime_mac_forever { get; set; }
+        public int Playtime_linux_forever { get; set; }
+        public int? Playtime_2weeks { get; set; }
+    }
+
+    public class Response
+    {
+        public int Game_count { get; set; }
+        public IList<Game> Games { get; set; }
+    }
+
+    public class Steam_GameList
+    {
+        public Response Response { get; set; }
+    }
+}
+
